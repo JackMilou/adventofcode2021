95467933c6262eb1d261ef47c0c8cf7b48ed6a7d
MilouSchipper <milou.schipper2004@gmail.com>
2021-12-02 11:29:29 +0100

    Initial commit

---
+++ adventofcode2021/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Linq;
+
+//http://localhost/adventofcode/2021/day1.1.php
+
+namespace adventofcode2021
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            var numbers = System.IO.File.ReadAllLines(@"C:\development\c#\HalloWereld\2021txt\day12021.txt").Select(number => int.Parse(number)).ToArray();
+
+            // int count = 0;
+            //
+            // for (int i = 0; i < numbers.Length; i++)
+            // {
+            //     int j = i + 1;
+            //     if (numbers[i] < numbers[j])
+            //     {
+            //         count++;
+            //     }
+            // }
+            
+            //Console.WriteLine(count);
+            
+            int count = 0;
+            
+            int beginnummer = numbers[0];
+            int i = 1;
+            int j = 0;
+            foreach (int number in numbers)
+            {
+                if (i < numbers.Length)
+                {
+                    if (numbers[i] > numbers[j])
+                    {
+                        count++;
+                    }
+                    i++;
+                    j++;
+                }
+            }
+            Console.WriteLine(count);
+
+            int countmeas = 0;
+            int k = 0;
+            foreach (var number in numbers)
+            {
+                if (k + 3 < numbers.Length)
+                {
+                    int measurement1 = numbers[k] + numbers[k + 1] + numbers[k + 2];
+                    int measurement2 = numbers[k + 1] + numbers[k + 2] + numbers[k + 3];
+
+                    if (measurement2 > measurement1)
+                    {
+                        countmeas++;
+                    }
+                }
+
+                k++;
+            }
+            Console.WriteLine(countmeas);
+
+
+
+
+        }
+        }
+    }
---
+++ day2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+
+namespace day2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = System.IO.File.ReadAllLines(@"C:\development\c#\HalloWereld\2021txt\day22021.txt").Select(o => o.Split(' '));
+
+            // string direction;
+            // int i;
+            // int x = 0;
+            // int y = 0;
+            // foreach (var instruction in numbers)
+            // {
+            //     direction = instruction[0];
+            //     i = int.Parse(instruction[1]);
+            //     switch (direction)
+            //     {
+            //         case "down":
+            //             y -= i;
+            //             break;
+            //         case "up":
+            //             y += i;
+            //             break;
+            //         case "forward":
+            //             x += i;
+            //             break;
+            //     }
+            //     
+            // }
+            // Console.WriteLine(x);
+            // Console.WriteLine(y);
+            //
+            // Console.WriteLine(x*y);
+            
+            string direction;
+            int aim = 0;
+            int i;
+            int x = 0;
+            int y = 0;
+            foreach (var instruction in numbers)
+            {
+                direction = instruction[0];
+                i = int.Parse(instruction[1]);
+                switch (direction)
+                {
+                    case "down":
+                        
+                        aim += i;
+                        break;
+                    case "up":
+                        
+                        aim -= i;
+                        break;
+                    case "forward":
+                        x += i;
+                        y -= i * aim;
+                        break;
+                }
+                
+            }
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+            Console.WriteLine(x*y);
+        }
+    }
+}
